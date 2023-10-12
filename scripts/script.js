601378e659dc8724a2b7999b0016f33d8fce7594
cnorwood10 <142539217+cnorwood10@users.noreply.github.com>
2023-10-12 15:38:44 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const classCharlies = [
+    
+  ];
+
+
+
+//   const data = [{
+//     id: 1,
+//     show: 'Suits',
+//     place: 'Peacock',
+//     status: 'Watching',
+//   }, {
+//     id: 2,
+//     show: 'Lucifer',
+//     place: 'Netflix',
+//     status: 'Finished',
+//   }, {
+//     id: 3,
+//     show: 'Outer Banks',
+//     place: 'Netflix',
+//     status: 'Caught Up',
+//   }, {
+//     id: 4,
+//     show: 'The Rookie',
+//     place: 'Hulu',
+//     status: 'Caught Up',
+//   }];
+
+
+// https://panhead63.github.io/friendly-octo-guide-delta/ Rachel Thompson
+// https://loganbeckman21.github.io/charlieLab2/ Logan Beckman
+// https://daltrodg.github.io/curly-disco-delta/ Dalton Rodgers
+// https://raidengreene.github.io/namefactsdeltafall2023/ Raiden Greene
